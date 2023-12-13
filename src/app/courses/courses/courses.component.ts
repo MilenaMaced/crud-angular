5bd619fb05fdeb43621205933aa690e09302b7ec
Milena Macedo <milena.macedo@mv.com.br>
2023-12-13 16:19:20 -0300

    Aula 18 - Adcionando botões e criando o formulário

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -4,6 +4,7 @@ import { CoursesService } from '../services/courses.service';
 import { Observable, catchError, of } from 'rxjs';
 import { MatDialog } from '@angular/material/dialog';
 import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-courses',
@@ -13,19 +14,28 @@ import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/err
 export class CoursesComponent {
 
   courses$: Observable<Course[]>;
-  displayedColumns = ['name', 'category'];
+  displayedColumns = ['name', 'category', 'actions'];
 
-  constructor(private courseService: CoursesService, public dialog: MatDialog) {
+  constructor(
+    private courseService: CoursesService,
+    public dialog: MatDialog,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
     this.courses$ = courseService.list().pipe(
-      catchError(error =>{
+      catchError(error => {
         this.onError('Error ao carregar cursos')
         return of([])
       })
     );
   }
-  onError(erroMsg: string){
-      this.dialog.open(ErrorDialogComponent, {
-        data: erroMsg
-      });
+  onError(erroMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: erroMsg
+    });
+  }
+
+  onAdd() {
+    this.router.navigate(['new'], { relativeTo: this.route })
   }
 }
