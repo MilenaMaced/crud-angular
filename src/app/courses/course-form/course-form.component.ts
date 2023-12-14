d6a552d6a365ca79a2fe96502370be5eeda206cb
Milena Macedo <milena.macedo@mv.com.br>
2023-12-14 10:06:58 -0300

    Aula 21 - Adcionando mensagem de sucesso

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { CoursesService } from '../services/courses.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-course-form',
@@ -15,7 +16,8 @@ export class CourseFormComponent {
   constructor(
     private formBuilder: FormBuilder,
     private service: CoursesService,
-    private _snackBar: MatSnackBar
+    private _snackBar: MatSnackBar,
+    private location: Location
   ) {
     this.form = this.formBuilder.group({
       name: [null],
@@ -24,15 +26,21 @@ export class CourseFormComponent {
   }
 
   onSubmit() {
-    this.service.save(this.form.value).subscribe(result =>
-      console.log(result)
-      , error => this.onError());
+    this.service.save(this.form.value).subscribe(
+      result => this.onSucess(),
+      error => this.onError()
+    );
   }
 
   onCancel() {
-
+    this.location.back();
   }
+
   private onError() {
     this._snackBar.open('Error ao cadastrar curso', '', { duration: 5000 });
+  }
+  private onSucess() {
+    this._snackBar.open('Curso salvo com curso', '', { duration: 5000 });
+    this.location.back();
   }
 }
