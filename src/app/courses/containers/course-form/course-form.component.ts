645a645b5094c24f32d1978cd03af9379b018f08
Milena Macedo <milena.macedo@mv.com.br>
2023-12-15 11:06:17 -0300

    Aula 34 - Validação de campos no formulário

--- src/app/courses/containers/course-form/course-form.component.ts
+++ src/app/courses/containers/course-form/course-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NonNullableFormBuilder } from '@angular/forms';
+import { NonNullableFormBuilder, Validators } from '@angular/forms';
 import { CoursesService } from '../../services/courses.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Location } from '@angular/common';
@@ -15,8 +15,12 @@ export class CourseFormComponent {
 
   form = this.formBuilder.group({
     id: [''],
-    name: [''],
-    category: ['']
+    name: ['', [
+      Validators.required,
+      Validators.minLength(5),
+      Validators.maxLength(100)
+    ]],
+    category: ['', Validators.required]
   });;
 
   constructor(
@@ -45,8 +49,28 @@ export class CourseFormComponent {
   private onError() {
     this._snackBar.open('Error ao cadastrar curso', '', { duration: 5000 });
   }
+
   private onSucess() {
     this._snackBar.open('Curso salvo com curso', '', { duration: 5000 });
     this.location.back();
+  }
+
+  getErrorMessage(fieldName: string) {
+    const field = this.form.get(fieldName);
+    if (field?.hasError('required')) {
+      return 'Campo Obrigatório!'
+    }
+
+    if (field?.hasError('minlength')) {
+      const requiredLength = field.errors ? field.errors['minlength']['requiredLength'] : 5;
+      return `Tamanho mínimo precisa ser de ${requiredLength} caracteres.`;
+    }
+
+    if (field?.hasError('maxlength')) {
+      const requiredLength = field.errors ? field.errors['maxlength']['requiredLength'] : 100;
+      return `Tamanho máximo excedido de ${requiredLength} caracteres.`;
+    }
+
+    return 'Campo Inválido';
   }
 }
