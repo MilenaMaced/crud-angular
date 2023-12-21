c3547701482b36d744741adde6428742d508b6e0
Milena Macedo <milena.macedo@mv.com.br>
2023-12-21 10:40:49 -0300

    Aula 60 -  adicionando paginação

--- src/app/courses/containers/courses/courses.component.ts
+++ src/app/courses/containers/courses/courses.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute, Router } from '@angular/router';
-import { catchError, Observable, of } from 'rxjs';
+import { catchError, Observable, of, tap } from 'rxjs';
 import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 
 import { Course } from '../../model/course';
 import { CoursesService } from '../../services/courses.service';
 import { ConfirmationDialogComponent } from 'src/app/shared/components/confirmation-dialog/confirmation-dialog.component';
+import { CoursePage } from '../../model/course-page';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-courses',
@@ -16,7 +18,11 @@ import { ConfirmationDialogComponent } from 'src/app/shared/components/confirmat
 })
 export class CoursesComponent {
 
-  courses$: Observable<Course[]> | null = null;
+  courses$: Observable<CoursePage> | null = null;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  pageIndex = 0;
+  pageSize = 10;
 
   constructor(
     private courseService: CoursesService,
@@ -26,6 +32,20 @@ export class CoursesComponent {
     private _snackBar: MatSnackBar,
   ) {
     this.refresh();
+  }
+
+  refresh(pageEvent: PageEvent = { length: 0, pageIndex: 0, pageSize: 10 }) {
+    this.courses$ = this.courseService.list(pageEvent.pageIndex, pageEvent.pageSize)
+      .pipe(
+        tap(() => {
+          this.pageIndex = pageEvent.pageIndex;
+          this.pageSize = pageEvent.pageSize;
+        }),
+        catchError(error => {
+          this.onError('Erro ao carregar cursos.');
+          return of({ courses: [], totalElements: 0, totalPages: 0 })
+        })
+      );
   }
 
   onError(erroMsg: string) {
@@ -64,14 +84,5 @@ export class CoursesComponent {
         }
       }
     });
-  }
-
-  refresh() {
-    this.courses$ = this.courseService.list().pipe(
-      catchError(error => {
-        this.onError('Error ao carregar cursos')
-        return of([])
-      })
-    );
   }
 }
--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Course } from '../model/course';
 import { HttpClient } from '@angular/common/http';
 import { first } from 'rxjs/operators';
+import { CoursePage } from '../model/course-page';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +12,8 @@ export class CoursesService {
   private readonly API = 'api/courses'
   constructor(private httpClient: HttpClient) { }
 
-  list() {
-    return this.httpClient.get<Course[]>(this.API).pipe(first());
+  list(page = 0, pageSize = 10) {
+    return this.httpClient.get<CoursePage>(this.API, { params: { page, pageSize } }).pipe(first());
   }
   save(record: Partial<Course>) {
     return record.id ? this.update(record) : this.create(record);
